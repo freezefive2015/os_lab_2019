ad83e55d380e10531992820a3940c24bead3ae3e
freeezefive <99074642+freezefive2015@users.noreply.github.com>
2022-12-17 01:25:15 +0300

    Delete lab6/src directory

--- lab6/src/lib.sh
+++
@@ -1,3 +0,0 @@
-gcc -c libmult.c -fPIC -o libmult.o
-gcc -shared libmult.o -o libmult.so
-LD_LIBRARY_PATH=$(pwd)
--- lab6/src/libmult.h
+++
@@ -1,3 +0,0 @@
-#include <netinet/in.h>
-#include <netinet/ip.h>
-uint64_t MultModulo(uint64_t a, uint64_t b, uint64_t mod);
